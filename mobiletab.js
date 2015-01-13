957ad86c81cd153f5ccf79cdba6b24a9b271e118
Frank <frank821204@gmail.com>
2015-01-13 13:47:22 +0800

    first commit

--- mobiletab.js
+++ mobiletab.js
@@ -1,74 +1,61 @@
+(function($){ 
+    'use strict'
+    $.fn.mobileTab = function(options) {
+          var defaults={'breakpoint':760,
+                       };
+          options=$.extend({}, defaults, options);
+          var $that=$(this),
+          	  that=this;
+          var $window=$(window)
+          var $desktopTitle=$('.mobiletab-desktop-nav');
+  		  var $mobileTitle=$('.mobiletab-content-title');
+  		  var $content=$('.mobiletab-content-block');
+  	      var currentIndex=0;
+          return this.each(function(index,element) {
+  			var $element=$(element);
+  			var init=function(breakpoint){
+  				if (breakpoint<options.breakpoint) {
+  					$mobileTitle.show().removeClass('active');
+  					$mobileTitle.eq(currentIndex).addClass('active');
+  					$desktopTitle.hide();
+  					$content.hide();
+  					$content.removeClass('active').eq(currentIndex).addClass('active').show();
+  				}
+  				else {
+  					$that.removeClass('active');
+  					$that.eq(currentIndex).addClass("active");
+  					$mobileTitle.hide();
+  					$desktopTitle.show();
+  					$content.hide();
+  					$content.eq(currentIndex).show();
+  				}
+  			}
+  			init($window.width());
+  			$element.click(function(){
+  				var $this=$(this);
+  				$that.removeClass('active');
+  				$this.addClass('active');
+  				$content.removeClass('active').hide();
+  				$content.eq(index).addClass('active').show();
+  				currentIndex=index;
+  			})
+  			$mobileTitle.each(function(index,element){
+  				var $element=$(element);
+  				$element.click(function(){
+  					$mobileTitle.removeClass('active');
+  					$(this).addClass('active');
+  					$content.removeClass('active').hide();
+  					$content.eq(index).addClass('active').show();
+  					currentIndex=index;
+  				})
+  			})
+  			$window.resize(function(){
+  				init($window.width());
+  			})
 
-jQuery.fn.extend({
-	mobileTab:function(options) {
-		var $=jQuery.noConflict();
-		$.fn.changeClass=function(removedClassName,addedClassName,index) {
-			this.removeClass(removedClassName)
-				.addClass(addedClassName)
-				.eq(index)
-				.removeClass(addedClassName)
-				.addClass(removedClassName);
-		};
-		var options=options||{};
-		var breakpoint=options.breakpoint||760;
-		var $window=$(window);
-		var $mobileTitle=$(".mobiletab-content-title");
-		var $content=$(".mobiletab-content");
-		var $blocks=$(".mobiletab-content-block");
-		var currentIndex=options.startIndex||0;
-		var $this=this;
-		var isMobile;
-		var init=function(){
-			if($window.width()<breakpoint) {
-				$this.hide();
-				$mobileTitle.show();
-				$content.show();
-				$blocks.removeClass("desktop").addClass("mobile");
-				isMobile=true;
-			}
-			else {
-				$this.show();
-				$mobileTitle.hide();
-				$content.removeAttr("style");
-				$blocks.removeClass("mobile").addClass("desktop");
-				isMobile=false;
-			}
-		}
-		var restore=function(index) {
-			$this.find("span").removeClass("active").eq(index).addClass("active");
-			$mobileTitle.removeClass("active").eq(index).addClass("active");
-			$blocks.changeClass("unfold","folded",index);
-		}
+  			
+  			}) 
+        return this;
+    }
+})(jQuery)
 
-		init();
-		(function(){
-			var nav=$this.find("span");
-			nav.each(function(index,element){
-				$(element).click(function(){
-					$content.changeClass("active","inactive",index);
-					nav.removeClass("active");
-					$(this).addClass("active");
-					currentIndex=index;
-				})
-			})
-			$mobileTitle.each(function(index,element){
-				$(element).click(function(){
-					$mobileTitle.removeClass("active");
-					$(this).addClass("active");
-					$blocks.changeClass("unfold","folded",index);
-					currentIndex=index;
-				})
-			})
-		})()
-		window.onresize=function(){
-			init();
-			if (isMobile) {
-				restore(currentIndex);
-			}
-			else {
-				restore(currentIndex);
-				$content.changeClass("active","inactive",currentIndex);
-			}
-		}
-	}
-})
